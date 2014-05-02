864fcdf7987c9c3fc88e38f0e8074f5e5620bca7
mnuzzolo <michael.nuzzolo@gmail.com>
2014-05-01 20:40:47 -0400

    add songs

---
+++ converter.cpp
@@ -0,0 +1,34 @@
+#include <list>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+  list <int> all_values;
+
+  int value = 0;
+  while(value != -1) {
+    cin >> std::hex >> value;
+  	
+    if(value == -1)
+  	 break;
+
+  	all_values.push_back(value/64);
+
+  }
+  
+  ofstream myfile;
+  myfile.open("wav_arr.txt");
+
+  myfile << "{ ";
+
+  for(list<int>::iterator it = all_values.begin(); it != all_values.end(); it++) {
+  	myfile << *it << ", ";
+  }
+
+  myfile << "0 }";
+  
+  myfile.close();
+}
